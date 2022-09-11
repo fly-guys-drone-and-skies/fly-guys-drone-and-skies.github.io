5019af67102512e9ef8b579685b662ed4425e47c
Evan Nolan <evannolanwork@gmail.com>
2022-09-11 16:34:40 -0400

    added Team.js

--- src/Team.js
+++ src/Team.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './Home.css';
 
 function Team() {
   return (
     <div class="home">
-        <h1>About the team </h1>
+        <h1>Project Roles: </h1>
+        <h2>Project Manager: Evan Nolan</h2>
+        <h2>Sponsor Communicator: Gavin Burris</h2>
+        <h2>Drone Handler: Joeseph Maldonado </h2>
+        <h2>Scrum Master: Derek Brower</h2>
+        <h2>Tech Lead: Chase Marino</h2>
     </div>
   );
 }
