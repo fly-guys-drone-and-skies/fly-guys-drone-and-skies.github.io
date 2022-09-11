17da79d43d94580a598beacad3d8e9df1b3d8b17
gb9951 <gb9951@g.rit.edu>
2022-09-11 16:13:59 -0400

    Created time sheet component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import TimeSheet from './TimeSheet'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>TEST ETSET TESTSETSETS</code> and save to reload.
         </p>
+        <TimeSheet />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/TimeSheet.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TimeSheet = () => {
+    return(
+        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTbNKMtDSma2B7ExI-jUGcFtXFsiunCOfrxISTfx7uVV98LBythlvralADgc4m1x6WMNeKquEeIPwLE/pubhtml?widget=true&amp;headers=false"></iframe>
+    );
+};
+
+export default TimeSheet;
