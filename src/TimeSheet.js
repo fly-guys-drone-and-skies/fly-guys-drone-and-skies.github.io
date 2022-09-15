2db4180ead3cdaf791d32ae6acbdaf971592e8e2
Chase Marino <cmarino@datto.com>
2022-09-15 18:18:54 -0400

    test

--- src/TimeSheet.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TimeSheet = () => {
-    return(
-        <iframe className="timeSheet" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTbNKMtDSma2B7ExI-jUGcFtXFsiunCOfrxISTfx7uVV98LBythlvralADgc4m1x6WMNeKquEeIPwLE/pubhtml?widget=true&amp;headers=false"></iframe>
-    );
-};
-
-export default TimeSheet;
